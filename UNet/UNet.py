6a7547e33484ced1326ec14a4212a0b3dee07a80
JustLikeComet <52590393+JustLikeComet@users.noreply.github.com>
2021-03-26 09:39:29 +0800

    Update UNet.py
    A script implements unet struct

--- UNet/UNet.py
+++ UNet/UNet.py
@@ -114,24 +114,11 @@ def upsampleSide(inputs, destchannels):
     print("6 out shape", unet_out6.get_shape().as_list())
     return unet_out6
 
-def adjustData(img,mask,flag_multi_class,num_class):
-    if(flag_multi_class):
-        img = img / 255
-        mask = mask[:,:,:,0] if(len(mask.shape) == 4) else mask[:,:,0]
-        new_mask = np.zeros(mask.shape + (num_class,))
-        for i in range(num_class):
-            #for one pixel in the image, find the class in mask and convert it into one-hot vector
-            #index = np.where(mask == i)
-            #index_mask = (index[0],index[1],index[2],np.zeros(len(index[0]),dtype = np.int64) + i) if (len(mask.shape) == 4) else (index[0],index[1],np.zeros(len(index[0]),dtype = np.int64) + i)
-            #new_mask[index_mask] = 1
-            new_mask[mask == i,i] = 1
-        new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1]*new_mask.shape[2],new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask,(new_mask.shape[0]*new_mask.shape[1],new_mask.shape[2]))
-        mask = new_mask
-    elif(np.max(img) > 1):
-        img = img / 255
-        mask = mask /255
-        mask[mask > 0.5] = 1
-        mask[mask <= 0.5] = 0
+def adjustData(img,mask):
+    img = img / 255
+    mask = mask /255
+    mask[mask > 0.5] = 1
+    mask[mask <= 0.5] = 0
     return (img,mask)
 
 
@@ -145,7 +132,7 @@ def geneTrainNpy(image_path,mask_path,flag_multi_class = False,num_class = 2,ima
         img = np.reshape(img,img.shape + (1,)) if image_as_gray else img
         mask = io.imread(item.replace(image_path,mask_path).replace(image_prefix,mask_prefix),as_gray = mask_as_gray)
         mask = np.reshape(mask,mask.shape + (1,)) if mask_as_gray else mask
-        img,mask = adjustData(img,mask,flag_multi_class,num_class)
+        img,mask = adjustData(img,mask)
         image_arr.append(img)
         mask_arr.append(mask)
     image_arr = np.array(image_arr)
